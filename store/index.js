a240e34ff8c6718e29ee36ec5e1338fa079f43cd
王真真 <wangzhenzhen@daling.com>
2021-07-13 19:15:24 +0800

    请求封装

--- store/index.js
+++ store/index.js
@@ -7,24 +7,37 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
 	state: {
 		loginState: false,
-		userInfo: uni.getStorageSync('user') ? uni.getStorageSync('user') : {
+		userInfo: uni.getStorageSync('user') || {
 			"id": "",
 			"created_at": "",
 			"mini_openid": "",
 			"union_id": "",
 			"edges": {}
 		},
+		jwt_data:uni.getStorageSync('jwt_data') || {
+			"expires_at":"",
+			"refresh_expires_at":"",
+			"refresh_token":"",
+			"token":""
+		},
 		cart: uni.getStorageSync('cart') ? JSON.parse(uni.getStorageSync('cart')) : [],
 		loginInitFn: []
 	},
 	getters: {},
 	mutations: {
+		jwtData(state,payload){
+			state.jwt_data = payload
+			uni.setStorageSync('jwt_data',payload)
+		},
 		loginInitFn(state, payload) {
 			state.loginInitFn = [...state.loginInitFn, payload]
 		},
 		userLogin(state, payload) {
+			// 登录状态
 			state.loginState = true
+			// 用户信息
 			state.userInfo = payload
+			// 缓存用户信息
 			uni.setStorageSync('user', payload)
 		},
 		userLogout(state) {
